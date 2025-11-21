e29ae75493862fab67be6b4e183101e77a65f55d
Damian Adamus <adamus.damian@yahoo.com>
2025-11-21 22:27:35 +0100

    Working on Playlists component

--- src/components/Playlists.jsx
+++ src/components/Playlists.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { useState } from "react";
+import { useMusic } from "../contexts/MusicContext";
 
-const Playlists = () => {
+export const Playlists = () => {
+  const [newPlaylistName, setNewPlaylistName] = useState("");
+  const [selectedPlaylist, setSelectedPlaylist] = useState(null);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const {
+    playlists,
+    createPlaylist,
+    allSongs,
+    addSongToPlaylist,
+    currentTrackIndex,
+    handlePlaySong,
+    deletePlaylist,
+  } = useMusic();
   return <div>Playlists</div>;
 };
-
-export default Playlists;
